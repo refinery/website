acd3c6bae5088ca7c57a0757c5ad8119774865f1
Brice Sanchez <contact@brice-sanchez.com>
2015-09-20 00:05:18 -0400

    Add basic HTTP auth during dev

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # TODO : Remove this basic HTTP authentication before delivery
+  before_action :authenticate, if: "Rails.env.production?"
+
+  protected
+
+    def authenticate
+      authenticate_or_request_with_http_basic do |username, password|
+        username == ENV["BHA_LOGIN"] && password == ENV["BHA_PASSWORD"]
+      end
+    end
 end
