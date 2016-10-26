84b7ddf545275c6865c667470d82508f14997d27
Philip Arndt <git@p.arndt.io>
2016-10-26 23:23:57 +1300

    Add better guide parsing to GuidesWorker

--- app/workers/guides_worker.rb
+++ app/workers/guides_worker.rb
@@ -1,5 +1,6 @@
 require "kramdown"
 require "octokit"
+require "base64"
 
 # LANG=en_US.UTF-8 kramdown -i GFM getting-started.md --no-hard-wrap --coderay-line-numbers table --smart-quotes apos,apos,quot,quot > getting-started.html
 
@@ -19,20 +20,80 @@ class GuidesWorker
   PATTERN = %r{\Adoc/guides/.*/*.md\z}.freeze
 
   def perform(sha = SHA, repo = REPO)
-    docs = github.tree(repo, sha, recursive: true).tree.select { |t| t.path =~ PATTERN }
+    @repo = repo
+    @sha = sha
 
     # No idea what to do with these yet, but here's the logic.
-    docs.map do |doc|
-      raw_doc_text = Base64.decode64(github.blob(repo, doc.sha).content)
-      html = Kramdown::Document.new(raw_doc_text, KRAMDOWN_OPTIONS).to_html
-      folder_name = doc.path.split('/')[-2]
-      title = doc.path.split('/').last.split(' - ').last.split('.md').first
+    docs.map(&method(:build_guide))
+  end
 
-      [html, folder_name, title]
+  def build_guide(doc)
+    guide = ::Refinery::Guides::Guide.where(
+      title: doc.path.split('/').last.split(' - ').last.split('.md').first,
+      sha: @sha
+    ).first_or_create
+
+    metadata, markdown = fetch_guide_markdown(doc)
+
+    guide.update_attributes(
+      :description => to_html(metadata["abstract"]),
+      :raw_source => markdown,
+      :html => to_html(markdown),
+      :category => doc.path.split('/')[-2],
+      :position => guide_position(doc.path.split("#{guide.category}/").last),
+      :source_url => "https://github.com/#{@repo}/blob/#{@sha}/#{doc.path.to_s.gsub(' ', '%20')}",
+      :sha => @sha
+    )
+    binding.pry
+
+    guide
+  end
+
+  def fetch_guide_markdown(doc)
+    raw = Base64.decode64(github.blob(@repo, doc.sha).content)
+    if raw =~ /\A---/
+      preamble = raw[/\A---.*?^---/m]
+      raw   = raw[preamble.size..-1]
+      metadata = YAML.load(preamble)
+    end
+    [metadata || {}, raw]
+  end
+
+  def to_html(raw_source)
+    return nil if raw_source.blank?
+
+    Kramdown::Document.new(raw_source, KRAMDOWN_OPTIONS).to_html
+  end
+
+  def guide_description(raw_source)
+    begin
+      raw_source.scan(/^(.*)endprologue\./m).flatten.first.to_s
+                .split("\n\n")[1..-1].to_a
+                .join("\n\n")
+    rescue
+      nil
+    end
+  end
+
+  def guide_position(title)
+    title.split(' - ').first.to_i
+  rescue
+    max_position
+  end
+
+  def max_position
+    ::Refinery::Guides::Guide.maximum(:position)
+  end
+
+  def docs
+    @docs ||= github.tree(@repo, @sha, recursive: true).tree.select do |t|
+      t.path =~ PATTERN
     end
   end
 
   def github
-    @github ||= Octokit::Client.new(access_token: Rails.application.secrets.guides_access_token)
+    @github ||= Octokit::Client.new(
+      access_token: Rails.application.secrets.guides_access_token
+    )
   end
 end
