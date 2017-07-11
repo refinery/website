78eee7f3706ca08731810b26d7579449d57d16ae
Brice Sanchez <contact@brice-sanchez.com>
2017-07-10 23:09:44 -0400

    Disable :fr locale in config.i18n.available_locales

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module RefineryWebsite
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :en
     config.i18n.fallbacks = [:en]
-    config.i18n.available_locales = [:en, :fr]
+    config.i18n.available_locales = [:en]
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
